a970fa5143de57f52bca4749604178b3f22219ce
Philip Cortes <philip.cortes@gmail.com>
2013-03-04 15:24:34 -0800

    Initial Commit

---
+++ app/models/purchase.rb
@@ -0,0 +1,7 @@
+class Purchase < ActiveRecord::Base
+  validates :name, :presence => true
+  validates :cost, :numericality => { :greater_than => 0 }
+  
+  attr_accessible :cost, :name
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThingsIBought::Application.config.secret_token = '7add6bb0d7a50d1e48ae4605cdc96fbd9516a3ec599cf0806ec2fbaea85abb354aeb9fb3540784df2bcf984f04bacccf72e585d2a52c53f215a08f9826c82aa2'
